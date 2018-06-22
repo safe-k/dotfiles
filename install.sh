f23143d06b54a03e9a9ab897b357305e841c7d09
Seif Kamal <sf@amp.co>
2018-06-22 11:13:27 +0100

    Add main installation script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "Staring installation.."
+
+# Path
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Homebrew
+## Install Homebrew if it doesn't exist
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Homebrew already installed"
+fi
+## Update homebrew recipes
+brew update
+
+# Source installation scripts in subdirectories
+for directory in $DOTFILES_DIR/*; do
+    if [ -d "$directory" ]; then
+        for file in $(find $directory -type f -name "install.sh"); do
+            source $file
+        done
+    fi
+done
+
