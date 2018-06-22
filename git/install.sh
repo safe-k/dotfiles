960703d08919f4d527880271c7d037616b22fe12
Seif Kamal <seifskm@gmail.com>
2018-06-22 12:17:06 +0100

    Add git config

---
+++ git/install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "Setting up Git.."
+
+# Check if Git is installed. If not, install it
+git --version 2>&1 >/dev/null
+GIT_IS_AVAILABLE=$?
+if [ $GIT_IS_AVAILABLE -eq 0 ]; then
+    echo "Installing Git.."
+    brew install git
+fi
+
+echo "Configuring Git..."
+
+# Configure Git
+echo "Username: "
+read username
+git config --global github.user $username
+
+echo "Name: "
+read name
+git config --global user.name $name
+
+echo "Email: "
+read email
+git config --global user.email $email
+
+git config --global core.editor "vim"
+git config --global core.autocrlf "input"
+git config --global help.autocorrect "1"
+
+# Install command line auto completion
+brew install bash-completion
+
