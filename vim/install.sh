45e01c50d43535a793c4b046d612670d0bad7304
Seif Kamal <seif@tickx.co.uk>
2020-10-26 16:12:10 +0000

    Create undo directory on install

--- vim/install.sh
+++ vim/install.sh
@@ -6,5 +6,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Symlink config
 ln -sfv "${DIR}/.vimrc" ~
 
+# Create undo directory
+mkdir -p ~/.vim/undo
+
 # Install plugins 
 vim +PlugInstall +qall > /dev/null
