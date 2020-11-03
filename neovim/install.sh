9f7615353517510793f1cbf57e33b2def6fbdcc2
Seif Kamal <seif@tickx.co.uk>
2020-11-03 16:23:44 +0000

    Install vim-plug with neovim

--- neovim/install.sh
+++ neovim/install.sh
@@ -5,6 +5,11 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_PATH="${XDG_DATA_HOME:-$HOME/.config}/nvim"
 mkdir -p "${CONFIG_PATH}"
 
+# Install vim-plug
+# See https://github.com/junegunn/vim-plug#neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 # Symlink config
 ln -sfv "${DIR}/init.vim" "${CONFIG_PATH}/init.vim"
 
